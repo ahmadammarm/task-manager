ebb60fb8b567603f50355f1b0bde6a2641049f0d
Ahmad Ammar Musyaffa <ahmad.ammar.2205356@students.um.ac.id>
2023-11-11 17:29:46 +0700

    update styling components

---
+++ app/components/Sidebar/Sidebar.tsx
@@ -0,0 +1,18 @@
+"use client"
+
+import React from 'react'
+import styled from 'styled-components'
+
+const SidebarStyled = styled.nav`
+
+`;
+
+const Sidebar = () => {
+  return (
+    <SidebarStyled>
+      Sidebar
+    </SidebarStyled>
+  )
+}
+
+export default Sidebar
