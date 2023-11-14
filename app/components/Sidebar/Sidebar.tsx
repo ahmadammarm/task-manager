7690b1d3678100327e6e23e4990180defb590764
Ahmad Ammar Musyaffa <ahmad.ammar.2205356@students.um.ac.id>
2023-11-14 07:02:08 +0700

    update sidebar

--- app/components/Sidebar/Sidebar.tsx
+++ app/components/Sidebar/Sidebar.tsx
@@ -8,11 +8,12 @@ import Image from 'next/image';
 import menu from '@/app/utils/menu';
 import Link from 'next/link';
 import { usePathname, useRouter } from 'next/navigation';
+import { arrowLeft, bars, logout } from "@/app/utils/Icons";
 
 
 const Sidebar = () => {
 
-  const { theme } = useGlobalState();
+  const { theme, collapsed, collapseMenu } = useGlobalState();
 
   const pathname = usePathname();
   const router = useRouter();
@@ -22,7 +23,10 @@ const Sidebar = () => {
   };
 
   return (
-    <SidebarStyled theme={theme}>
+    <SidebarStyled theme={theme} collapsed={collapsed}>
+      <button className="toggle-nav" onClick={collapseMenu}>
+        {collapsed ? bars : arrowLeft}
+      </button>
       <div className="profile">
         <div className="profile-overlay"></div>
         <div className="image">
@@ -55,25 +59,78 @@ const Sidebar = () => {
   )
 }
 
-const SidebarStyled = styled.nav`
+const SidebarStyled = styled.nav<{ collapsed: boolean }>`
   position: relative;
-  background-color: ${(props) => props.theme.colorBg2};
   width: ${(props) => props.theme.sidebarWidth};
+  background-color: ${(props) => props.theme.colorBg2};
   border: 2px solid ${(props) => props.theme.borderColor2};
   border-radius: 1rem;
+
   display: flex;
   flex-direction: column;
   justify-content: space-between;
+
   color: ${(props) => props.theme.colorGrey3};
-  
+
+  @media screen and (max-width: 768px) {
+    position: fixed;
+    height: calc(100vh - 2rem);
+    z-index: 100;
+
+    transition: all 0.3s cubic-bezier(0.53, 0.21, 0, 1);
+    transform: ${(props) =>
+      props.collapsed ? "translateX(-107%)" : "translateX(0)"};
+
+    .toggle-nav {
+      display: block !important;
+    }
+  }
+
+  .toggle-nav {
+    display: none;
+    padding: 0.8rem 0.9rem;
+    position: absolute;
+    right: -69px;
+    top: 1.8rem;
+
+    border-top-right-radius: 1rem;
+    border-bottom-right-radius: 1rem;
+
+    background-color: ${(props) => props.theme.colorBg2};
+    border-right: 2px solid ${(props) => props.theme.borderColor2};
+    border-top: 2px solid ${(props) => props.theme.borderColor2};
+    border-bottom: 2px solid ${(props) => props.theme.borderColor2};
+  }
+
+  .user-btn {
+    .cl-rootBox {
+      width: 100%;
+      height: 100%;
+
+      .cl-userButtonBox {
+        width: 100%;
+        height: 100%;
+
+        .cl-userButtonTrigger {
+          width: 100%;
+          height: 100%;
+          opacity: 0;
+        }
+      }
+    }
+  }
+
   .profile {
-    margin: 1rem;
+    margin: 1.5rem;
     padding: 1rem 0.8rem;
     position: relative;
+
     border-radius: 1rem;
     cursor: pointer;
-    font-weight: 600; 
+
+    font-weight: 500;
     color: ${(props) => props.theme.colorGrey0};
+
     display: flex;
     align-items: center;
 
@@ -84,10 +141,12 @@ const SidebarStyled = styled.nav`
       width: 100%;
       height: 100%;
       backdrop-filter: blur(10px);
-      border-radius: 1rem;
       z-index: 0;
-      background: ${(props) => props.theme.colorBg1};
-      transition: all 0.5s linear;
+      background: ${(props) => props.theme.colorBg3};
+      transition: all 0.55s linear;
+      border-radius: 1rem;
+      border: 2px solid ${(props) => props.theme.borderColor2};
+
       opacity: 0.2;
     }
 
@@ -95,10 +154,10 @@ const SidebarStyled = styled.nav`
       font-size: 1.2rem;
       display: flex;
       flex-direction: column;
-      margin-left: 1rem;
+
       line-height: 1.4rem;
     }
-    
+
     .image,
     h1 {
       position: relative;
@@ -108,21 +167,22 @@ const SidebarStyled = styled.nav`
     .image {
       flex-shrink: 0;
       display: inline-block;
+      overflow: hidden;
+      transition: all 0.5s ease;
+      border-radius: 100%;
+
       width: 70px;
       height: 70px;
-      border-radius: 100%;
-      transition: all 0.5s ease;
-      overflow: hidden;
-    }
 
-    img {
-      border-radius: 100%;
-      transition: all 0.5s ease;
+      img {
+        border-radius: 100%;
+        transition: all 0.5s ease;
+      }
     }
 
     > h1 {
-      margin-left: 1rem;
-      font-size: clamp(1.2rem, 4w, 1.3rem);
+      margin-left: 0.8rem;
+      font-size: clamp(1.2rem, 4vw, 1.4rem);
       line-height: 100%;
     }
 
@@ -138,6 +198,77 @@ const SidebarStyled = styled.nav`
     }
   }
 
+  .nav-item {
+    position: relative;
+    padding: 0.8rem 1rem 0.9rem 2.1rem;
+    margin: 0.3rem 0;
+
+    display: grid;
+    grid-template-columns: 40px 1fr;
+    cursor: pointer;
+    align-items: center;
+
+    &::after {
+      position: absolute;
+      content: "";
+      left: 0;
+      top: 0;
+      width: 0;
+      height: 100%;
+      background-color: ${(props) => props.theme.activeNavLinkHover};
+      z-index: 1;
+      transition: all 0.3s ease-in-out;
+    }
+
+    &::before {
+      position: absolute;
+      content: "";
+      right: 0;
+      top: 0;
+      width: 0%;
+      height: 100%;
+      background-color: ${(props) => props.theme.colorGreenDark};
+
+      border-bottom-left-radius: 5px;
+      border-top-left-radius: 5px;
+    }
+
+    a {
+      font-weight: 500;
+      transition: all 0.3s ease-in-out;
+      z-index: 2;
+      line-height: 0;
+    }
+
+    i {
+      display: flex;
+      align-items: center;
+      color: ${(props) => props.theme.colorIcons};
+    }
+
+    &:hover {
+      &::after {
+        width: 100%;
+      }
+    }
+  }
+
+  .active {
+    background-color: ${(props) => props.theme.activeNavLink};
+
+    i,
+    a {
+      color: ${(props) => props.theme.colorIcons2};
+    }
+  }
+
+  .active::before {
+    width: 0.3rem;
+  }
+
+  > button {
+    margin: 1.5rem;
+  }
 `;
 
 export default Sidebar
